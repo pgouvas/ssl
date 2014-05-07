0ac526a65f23cfaa3f79ada14fb6a6c1c6b5b302
Panagiotis Gouvas <pgouvas@ubitech.eu>
2014-05-07 11:24:51 +0300

    Update Credentials.java

--- src/util/Credentials.java
+++ src/util/Credentials.java
@@ -3,8 +3,8 @@ package util;
 public final class Credentials {
 
     public static final String VOIBUSTER_USERNAME   = "pgouvas";
-    public static final String VOIBUSTER_PASSWORD   = "c4mb8663";
+    public static final String VOIBUSTER_PASSWORD   = "cxxxxxxxxx";
     public static final String VOIBUSTER_FROM       = "pgouvas";
-    public static final String GOOGLE_PUSH_API_KEY  = "AIzaSyBfP5r1HK5kA6WS-VaoYMADCN3W7hp1vAo";
+    public static final String GOOGLE_PUSH_API_KEY  = "xxxxxxxxxxx";
 
 }//EoC Credentials
